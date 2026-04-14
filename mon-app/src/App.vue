7bcd7e5cf16b6e2df78ebb8042616922a879effc
Vito Outard <ilan.sah59@gmail.com>
2026-04-14 14:11:48 +0200

    css changé

--- mon-app/src/App.vue
+++ mon-app/src/App.vue
@@ -4,8 +4,15 @@ const today = new Date().toLocaleDateString('fr-FR')
 </script>
 <template>
 <header>
-<p>Nom : SAHRAOUI ILAN</p>
+<h1>Nom : SAHRAOUI ILAN</h1>
 <p>Date : {{ today }}</p>
 <p>Build : {{ buildId }}</p>
 </header>
 </template>
+
+<style>
+
+h1 {
+  color: red;
+}
+</style>
